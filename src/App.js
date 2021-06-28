4b1c5ef3d4d77775b6442c372030fb19ffc9eec4
tsopgniduhamel <tsopgniduhamel@gmail.com>
2021-06-28 23:30:02 +0100

    just the test for the commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <p>I just want to see if the modifications will be directed pushed in the github repo</p>
     </div>
   );
 }
